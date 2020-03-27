638c4177bf3775f247a726a9331e6f8cf6c63476
andre <andresoranzzo@gmail.com>
2020-03-27 15:03:20 -0300

    third commit

--- app/src/androidTest/java/com/example/helloworld/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/helloworld/ExampleInstrumentedTest.java
@@ -22,6 +22,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
 
-        assertEquals("com.example.helloworld2", appContext.getPackageName());
+        assertEquals("com.example.helloworld", appContext.getPackageName());
     }
 }
